f409d6aef4df604333f18c8b1eb649cbf21f2ca8
Marie JONES <marie.jones@corp.ovh.com>
2020-12-10 17:08:01 +0100

    refactor(confirm-termination): move form to reusable component
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/billing/src/index.js
+++ packages/manager/modules/billing/src/index.js
@@ -1,11 +1,12 @@
 import angular from 'angular';
 
+import cancellationForm from './components/cancellation-form';
 import servicesActions from './components/services-actions';
 import { RENEW_URL } from './components/services-actions/service-actions.constants';
 
 const moduleName = 'ovhManagerBilling';
 
-angular.module(moduleName, [servicesActions]);
+angular.module(moduleName, [cancellationForm, servicesActions]);
 
 export { RENEW_URL };
 
