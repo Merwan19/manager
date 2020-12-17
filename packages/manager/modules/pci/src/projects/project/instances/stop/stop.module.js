93d434259a66650f789fd3133a4217addaa090ff
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2020-12-17 10:21:37 +0100

    feat(pci/instances/stop): add stop instance feature
    ref: MANAGER-6080
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/instances/stop/stop.module.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+import '@ovh-ux/ng-translate-async-loader';
+import '@uirouter/angularjs';
+import 'angular-translate';
+import '@ovh-ux/ui-kit';
+import 'ovh-api-services';
+
+import stopInstance from '../instance/stop/stop.module';
+import routing from './stop.routing';
+
+const moduleName = 'ovhManagerPciInstancesStop';
+
+angular
+  .module(moduleName, [
+    stopInstance,
+    'ui.router',
+    'oui',
+    'ovh-api-services',
+    'ngTranslateAsyncLoader',
+    'pascalprecht.translate',
+  ])
+  .config(routing);
+
+export default moduleName;
