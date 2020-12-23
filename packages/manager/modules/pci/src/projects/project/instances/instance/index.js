36492455fbe5ff30176f5f8a22d53f507868beb8
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2020-12-23 10:04:00 +0100

    feat(pci/instances/instance): setup upsell cross-sell component
    ref: MANAGER-6076
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/index.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/index.js
@@ -1,22 +1,25 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
+import ovhManagerUpSellCrossSell from '@ovh-ux/manager-up-sell-cross-sell';
 
 const moduleName = 'ovhManagerPciInstanceLazyLoading';
 
-angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
-  /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('pci.projects.project.instances.instance.**', {
-      url: '/:instanceId',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+angular
+  .module(moduleName, ['ui.router', 'oc.lazyLoad', ovhManagerUpSellCrossSell])
+  .config(
+    /* @ngInject */ ($stateProvider) => {
+      $stateProvider.state('pci.projects.project.instances.instance.**', {
+        url: '/:instanceId',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-        return import('./instance.module').then((mod) =>
-          $ocLazyLoad.inject(mod.default || mod),
-        );
-      },
-    });
-  },
-);
+          return import('./instance.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+    },
+  );
 
 export default moduleName;
